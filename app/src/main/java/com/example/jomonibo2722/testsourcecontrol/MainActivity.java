9925e94da921e5e03013e0f794a791e3f57f81b8
Jeffrey Omonibo <Jomonibo2722@conestogac.on.ca>
2016-09-14 12:19:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jomonibo2722/testsourcecontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jomonibo2722.testsourcecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
